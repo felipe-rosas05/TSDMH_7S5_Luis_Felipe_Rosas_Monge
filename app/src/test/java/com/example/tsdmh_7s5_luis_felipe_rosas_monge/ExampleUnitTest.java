9b4d27b33c5a30a3648a4d4d313260068247c194
Pipe <felipaus500@gmail.com>
2026-09-05 22:51:27 -0600

    Initial commit

---
+++ app/src/test/java/com/example/tsdmh_7s5_luis_felipe_rosas_monge/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.tsdmh_7s5_luis_felipe_rosas_monge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
